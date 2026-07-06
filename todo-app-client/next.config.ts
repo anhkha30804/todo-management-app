68d477ce44b17167d91030ac4ca152fd23d55d97
anhkha30804 <anhkha30804@gmail.com>
2026-07-06 14:34:21 +0700

    chore: temp fix

--- todo-app-client/next.config.ts
+++ todo-app-client/next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: process.env.VERCEL ? undefined : 'standalone',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/board',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
